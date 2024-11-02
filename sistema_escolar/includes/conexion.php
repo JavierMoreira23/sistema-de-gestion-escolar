712a4d62653191b11d70d7380021afdc67204233
Javier Moreira <javier10moreira@hotmail.com>
2024-11-02 12:31:58 -0500

    agregando la carpeta administrador con todas sus carpetas

--- sistema_escolar/includes/conexion.php
+++ sistema_escolar/includes/conexion.php
@@ -5,5 +5,9 @@ $db= 'sistema-escolar';
 $pass = '';
 
     try{
-        
+        $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pass);
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    }catch(Exception $e){
+        echo 'Error: ' .$e ->getMessage();
     }
