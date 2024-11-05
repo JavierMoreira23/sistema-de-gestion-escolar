0fbc63f462ee7b5585f4455c6a31fbcc06bd8d78
Javier Moreira <javier10moreira@hotmail.com>
2024-11-05 11:17:28 -0500

    codigo de redireccion de paginas una vez validado los datos

--- sistema_escolar/index.php
+++ sistema_escolar/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.12/dist/sweetalert2.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
 
     <title>SISTEMA DE CURSOS</title>
 </head>
@@ -35,6 +35,7 @@
                                 <input type="text" name="usuario" id="usuario" placeholder="Nombre del Usuario"->
                                 <label for="password">CONTRASEÑA</label>
                                 <input type="password" name="pass" id="pass" placeholder="Contraseña">
+                                <div id="messageUsuario"></div>
                                 <button id="loginUsuario" type="button">INICIAR SESION</button>
 
                             </form>
@@ -46,6 +47,7 @@
                                 <input type="text" name="usuario" id="usuario" placeholder="Nombre del Usuario"->
                                 <label for="password">CONTRASEÑA</label>
                                 <input type="password" name="pass" id="pass" placeholder="Contraseña">
+                                <div id="messageProfesor"></div>
                                 <button id="loginProfesor" type="button">INICIAR SESION</button>
 
                             </form>
@@ -53,8 +55,9 @@
                     </div>
             </div>
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.12/dist/sweetalert2.min.js"></script>
-    <script src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="js/login.js"></script>
+    <script src="js/jquery-3.7.0.min.js"></script>
     </body>
 
 </html>
